53adf3d74dc7f2406876710872084e2b22095130
Koo In Young <zerokoo@lviscorp.com>
2023-07-10 20:18:37 +0900

    fix: Add Icon props to the Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { Component } from "react";
 import clsx from "clsx";
 import { Size, Theme } from "@/types/style";
 
@@ -6,6 +7,8 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   theme?: Theme;
   size?: Size;
+  LeftIcon?: React.FC<{ size?: number }>;
+  RightIcon?: React.FC<{ size?: number }>;
   ghost?: boolean;
   outline?: boolean;
   link?: boolean;
@@ -21,6 +24,8 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
 export function Button({
   theme = "neutral",
   size = "sm",
+  LeftIcon,
+  RightIcon,
   ghost = false,
   outline = false,
   glass = false,
@@ -37,8 +42,9 @@ export function Button({
 }: ButtonProps) {
   return (
     <button
+      type="button"
       className={clsx(
-        "btn",
+        "btn flex",
         {
           "btn-neutral": theme === "neutral",
           "btn-primary": theme === "primary",
@@ -48,10 +54,10 @@ export function Button({
           "btn-success": theme === "success",
           "btn-warning": theme === "warning",
           "btn-error": theme === "error",
-          "btn-lg": size === "lg",
-          "btn-md": size === "md",
-          "btn-sm": size === "sm",
-          "btn-xs": size === "xs",
+          "btn-lg gap-3": size === "lg",
+          "btn-md gap-2": size === "md",
+          "btn-sm gap-1.5": size === "sm",
+          "btn-xs gap-1": size === "xs",
           "btn-block": block,
           "btn-ghost": ghost,
           "btn-link": link,
@@ -61,13 +67,30 @@ export function Button({
           "btn-circle": circle,
           "btn-active": active,
           "btn-disabled": disabled,
+          "pl-4": !!LeftIcon && size === "lg",
+          "pl-3": !!LeftIcon && size === "md",
+          "pl-2": !!LeftIcon && size === "sm",
+          "pl-1": !!LeftIcon && size === "xs",
+          "pr-4": !!RightIcon && size === "lg",
+          "pr-3": !!RightIcon && size === "md",
+          "pr-2": !!RightIcon && size === "sm",
+          "pr-1": !!RightIcon && size === "xs",
           glass: glass,
         },
         className
       )}
       {...rest}
     >
-      {children}
+      {LeftIcon && <LeftIcon size={iconSizeMap[size]} />}
+      <span>{children}</span>
+      {RightIcon && <RightIcon size={iconSizeMap[size]} />}
     </button>
   );
 }
+
+const iconSizeMap: Record<Size, number> = {
+  lg: 26,
+  md: 22,
+  sm: 18,
+  xs: 14,
+};
