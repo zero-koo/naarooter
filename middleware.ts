8a92533ab7867a506483dc6a563b0ba1580901a7
Koo In Young <zerokoo@lviscorp.com>
2023-10-25 20:49:29 +0900

    feat: Setup basic clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/poll/(.+)', '/api/trpc/poll.(list|byId)(.*)'],
+  publicRoutes: [
+    '/',
+    '/poll/(.+)',
+    '/api/trpc/poll.(list|byId)(.*)',
+    '/api/webhook',
+  ],
 });
 
 export const config = {
