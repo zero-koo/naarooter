757ef0c706b50a2c7884be054e9d2858c16e4d63
Koo In Young <zerokoo@lviscorp.com>
2023-11-02 22:35:37 +0900

    feat: Add prisma seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 async function main() {
-  const zero = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'berksmile@gmail.com' },
     update: {},
     create: {
@@ -18,6 +18,7 @@ async function main() {
       polls: {
         create: [
           {
+            id: 'c84d8367-460a-4772-b2dd-42170110d774',
             title: '혈액형 vs MBTI',
             description: '혈액형은 성격과 관련이 있을까?',
             choices: {
@@ -50,6 +51,7 @@ async function main() {
             },
           },
           {
+            id: '0249a98f-b570-4e04-b89c-950e13650bc8',
             title: '김치찌개 vs 된장찌개',
             description: '',
             choices: {
@@ -70,6 +72,7 @@ async function main() {
             },
           },
           {
+            id: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
             title: '시험 컨닝해본적 있다 / 없다',
             description: '',
             choices: {
@@ -90,6 +93,7 @@ async function main() {
             },
           },
           {
+            id: '6fb35c63-1b55-4e53-bf31-98e198d28667',
             title: '민트초코 호불호',
             description: '',
             choices: {
@@ -114,15 +118,19 @@ async function main() {
       votes: {
         create: [
           {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
             pollChoiceId: 'bf02d7ef-e916-41ed-a36f-0efc9c8cefd2',
           },
           {
+            pollId: '0249a98f-b570-4e04-b89c-950e13650bc8',
             pollChoiceId: '40ec1f12-6415-49b2-aa58-8ec58944edcc',
           },
           {
+            pollId: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
             pollChoiceId: '2c882f62-f9d4-491d-b4ca-74ade4a5bc49',
           },
           {
+            pollId: '6fb35c63-1b55-4e53-bf31-98e198d28667',
             pollChoiceId: 'dbb97f12-4b9b-4e2e-8dd3-126b9e1684d2',
           },
         ],
@@ -146,15 +154,19 @@ async function main() {
       votes: {
         create: [
           {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
             pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
           },
           {
+            pollId: '0249a98f-b570-4e04-b89c-950e13650bc8',
             pollChoiceId: '40ec1f12-6415-49b2-aa58-8ec58944edcc',
           },
           {
+            pollId: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
             pollChoiceId: 'dbb97f12-4b9b-4e2e-8dd3-126b9e1684d2',
           },
           {
+            pollId: '6fb35c63-1b55-4e53-bf31-98e198d28667',
             pollChoiceId: '4b6b9586-6224-4b4a-9299-459ea28c259a',
           },
         ],
@@ -177,15 +189,19 @@ async function main() {
       votes: {
         create: [
           {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
             pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
           },
           {
+            pollId: '0249a98f-b570-4e04-b89c-950e13650bc8',
             pollChoiceId: '40ec1f12-6415-49b2-aa58-8ec58944edcc',
           },
           {
+            pollId: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
             pollChoiceId: 'df939951-da0f-4686-a86c-76a185e2da56',
           },
           {
+            pollId: '6fb35c63-1b55-4e53-bf31-98e198d28667',
             pollChoiceId: 'dbb97f12-4b9b-4e2e-8dd3-126b9e1684d2',
           },
         ],
@@ -208,22 +224,405 @@ async function main() {
       votes: {
         create: [
           {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
             pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
           },
           {
+            pollId: '0249a98f-b570-4e04-b89c-950e13650bc8',
             pollChoiceId: '40ec1f12-6415-49b2-aa58-8ec58944edcc',
           },
           {
-            pollChoiceId: 'df939951-da0f-4686-a86c-76a185e2da56',
+            pollId: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
+            pollChoiceId: 'dbb97f12-4b9b-4e2e-8dd3-126b9e1684d2',
           },
           {
+            pollId: '6fb35c63-1b55-4e53-bf31-98e198d28667',
             pollChoiceId: 'dbb97f12-4b9b-4e2e-8dd3-126b9e1684d2',
           },
         ],
       },
     },
   });
-  console.log({ zero });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+3@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+3@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'N',
+          ft: 'T',
+          jp: 'P',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+          {
+            pollId: '0249a98f-b570-4e04-b89c-950e13650bc8',
+            pollChoiceId: '2a3f12be-2a93-42e6-abe2-3f677e544349',
+          },
+          {
+            pollId: 'fc0fc980-94c2-481c-930f-06a7c2dee1ba',
+            pollChoiceId: 'df939951-da0f-4686-a86c-76a185e2da56',
+          },
+          {
+            pollId: '6fb35c63-1b55-4e53-bf31-98e198d28667',
+            pollChoiceId: '2c882f62-f9d4-491d-b4ca-74ade4a5bc49',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+4@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+4@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'S',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+5@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+5@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'N',
+          ft: 'F',
+          jp: 'P',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+6@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+6@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'N',
+          ft: 'F',
+          jp: 'P',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'bf02d7ef-e916-41ed-a36f-0efc9c8cefd2',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+7@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+7@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'S',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+7@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+7@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'S',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+8@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+8@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'S',
+          ft: 'F',
+          jp: 'P',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+9@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+9@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'N',
+          ft: 'T',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '50cd9d61-6d8a-40fa-8d13-e7e118fa37de',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+10@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+10@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'N',
+          ft: 'T',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+11@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+11@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'N',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+12@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+12@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'N',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+13@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+13@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'I',
+          sn: 'N',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+14@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+14@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'N',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: '36bf06ca-853c-4729-b1bf-a7ee5968059d',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+15@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+15@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'N',
+          ft: 'F',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+16@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+16@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'S',
+          ft: 'T',
+          jp: 'P',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
+  await prisma.user.upsert({
+    where: { email: 'inzerokoo+17@gmail.com' },
+    update: {},
+    create: {
+      email: 'inzerokoo+17@gmail.com',
+      UserMbti: {
+        create: {
+          ei: 'E',
+          sn: 'S',
+          ft: 'T',
+          jp: 'J',
+        },
+      },
+      votes: {
+        create: [
+          {
+            pollId: 'c84d8367-460a-4772-b2dd-42170110d774',
+            pollChoiceId: 'cd90f625-d40b-4964-9fa0-b1857cb8ec4e',
+          },
+        ],
+      },
+    },
+  });
 }
 main()
   .then(async () => {
