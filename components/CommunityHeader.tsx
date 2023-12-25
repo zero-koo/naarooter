1beb5e5850606e5ab0ff0a4349756893221f39e2
Koo In Young <inzerokoo@gmail.com>
2023-12-25 14:32:00 +0900

    feat: Implement NavMenu toggle

--- components/CommunityHeader.tsx
+++ components/CommunityHeader.tsx
@@ -1,5 +1,7 @@
 import { MenuIcon } from 'lucide-react';
 import { ReactNode } from 'react';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './ui/Sheet';
+import GlobalNavigation from './nav/GlobalNavigation';
 
 type CommunityHeaderProps = {
   title: ReactNode;
@@ -10,11 +12,7 @@ type CommunityHeaderProps = {
 const CommunityHeader = ({ title, left, right }: CommunityHeaderProps) => {
   return (
     <div className="relative flex h-12 items-center justify-between bg-base-300">
-      {left ?? (
-        <button className="flex h-12 w-12 items-center justify-center">
-          <MenuIcon />
-        </button>
-      )}
+      {left ?? <NavMenuToggle />}
       <h1 className="absolute left-1/2 flex -translate-x-1/2 items-center text-lg font-semibold">
         {title}
       </h1>
@@ -24,3 +22,18 @@ const CommunityHeader = ({ title, left, right }: CommunityHeaderProps) => {
 };
 
 export default CommunityHeader;
+
+const NavMenuToggle = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="flex h-12 w-12 items-center justify-center">
+        <MenuIcon />
+      </SheetTrigger>
+      <SheetContent side={'left'} className="w-[250px]">
+        <div className="p-2 py-3">
+          <GlobalNavigation />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
