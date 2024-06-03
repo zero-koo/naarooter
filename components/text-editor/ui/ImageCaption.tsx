02a5d0460d453c71ec694a2bdcc4ad5cd82ab2c6
Koo In Young <inzerokoo@gmail.com>
2024-06-03 14:39:03 +0900

    fix: Focus caption input when it's toggled on

--- components/text-editor/ui/ImageCaption.tsx
+++ components/text-editor/ui/ImageCaption.tsx
@@ -23,22 +23,36 @@ export const ImageCaption = ({
     contentEditableRef.current.textContent = initialValue;
   }, [contentEditableRef, initialValue]);
 
+  const prevHasCaption = useRef<boolean>(hasCaption);
+  useEffect(() => {
+    if (prevHasCaption.current === hasCaption) return;
+
+    // Need to Debug
+    setTimeout(() => contentEditableRef.current?.focus(), 100);
+  }, [hasCaption]);
+
+  useEffect(() => {
+    prevHasCaption.current = hasCaption;
+  }, [hasCaption]);
+
   if (!hasCaption) return null;
   return (
-    <div
-      className="mt-1 px-0.5 text-xs font-semibold"
-      ref={contentEditableRef}
-      contentEditable={!readonly}
-      suppressContentEditableWarning
-      onInput={(e) => {
-        onChangeCaption?.(e.currentTarget.innerHTML);
-      }}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      }}
-    />
+    <div className="relative">
+      <div
+        className="mt-1 px-0.5 text-xs font-semibold empty:before:opacity-50 empty:before:content-['캡션을_입력하세요']"
+        ref={contentEditableRef}
+        contentEditable={!readonly}
+        suppressContentEditableWarning
+        onInput={(e) => {
+          onChangeCaption?.(e.currentTarget.innerHTML);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            e.stopPropagation();
+          }
+        }}
+      />
+    </div>
   );
 };
