e400444058474ba8f122571ac0fbf224f54ee4ec
Koo In Young <inzerokoo@gmail.com>
2024-07-19 12:57:13 +0900

    fix: Enhance PostSkeleton UI

--- components/skeletons/PostSkeleton.tsx
+++ components/skeletons/PostSkeleton.tsx
@@ -1,10 +1,11 @@
+import GrayBox from '../ui/GrayBox';
 import Loading from '../ui/Loading';
 import Skeleton from '../ui/Skeleton';
 
 const PostSkeleton = () => {
   return (
     <div className="flex h-full flex-col gap-3 py-0.5">
-      <div className="space-y-2.5 px-3 py-5">
+      <GrayBox className="space-y-2.5 p-3">
         <Skeleton className="h-6 w-full" />
         <div className="flex gap-1">
           <Skeleton className="h-4 w-12" />
@@ -12,7 +13,7 @@ const PostSkeleton = () => {
           <Skeleton className="ml-auto h-4 w-10" />
           <Skeleton className="h-4 w-10" />
         </div>
-      </div>
+      </GrayBox>
       <div className="flex-center flex-1 py-10">
         <Loading />
       </div>
