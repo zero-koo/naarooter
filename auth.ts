f36b1dc50799e4713e6a555cd0ad1a9968e1e50e
Koo In Young <inzerokoo@gmail.com>
2024-07-19 12:48:54 +0900

    feat: Initial name setting when first login

--- auth.ts
+++ auth.ts
@@ -7,6 +7,29 @@ import { prisma } from './server/prisma';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
+  events: {
+    async signIn({ user, isNewUser }) {
+      if (!isNewUser) return;
+
+      const userData = await prisma.user.findUnique({
+        where: {
+          id: user.id,
+        },
+      });
+      if (!userData) {
+        throw Error('User data should exist!');
+      }
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          name: `user_${userData.id.substring(16)}`,
+        },
+      });
+    },
+  },
+
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
