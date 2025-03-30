85ce18d0664a3ade21ec0f2cecd869da9a425893
Koo In Young <zerokoo@gu-in-yeong-ui-Macmini.local>
2025-03-30 22:06:00 +0900

    feat: Redirect to the community page after it is created

--- components/community/CommunityForm/index.tsx
+++ components/community/CommunityForm/index.tsx
@@ -1,15 +1,25 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { api } from '@/trpc/react';
+import { useQueryClient } from '@tanstack/react-query';
+import { getQueryKey } from '@trpc/react-query';
 
 import CommunitFormView from './CommunityForm';
 
 const CommunityForm = ({ onSubmit }: { onSubmit?: () => void }) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
   api.community.topics.useQuery();
   const { mutate: requestCreateCommunity, isPending: isSubmitting } =
     api.community.create.useMutation({
-      onSuccess() {
+      onSuccess(data) {
         onSubmit?.();
+        queryClient.invalidateQueries({
+          queryKey: getQueryKey(api.community.myList),
+        });
+        router.push(`/community/${data.id}`);
       },
     });
 
